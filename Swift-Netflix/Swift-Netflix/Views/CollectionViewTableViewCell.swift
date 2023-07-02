7294bb517c9783d8325b7d643434429bbb6b7728
Okariuss <orkunokann@gmail.com>
2023-07-02 12:47:33 +0300

    Created TableView on HomeViewController

---
+++ Swift-Netflix/Swift-Netflix/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Swift-Netflix
+//
+//  Created by Okan Orkun on 2.07.2023.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
