d4fa904ca1d97f9df540ae769241d48ba5b7092e
Okariuss <orkunokann@gmail.com>
2023-07-02 12:05:56 +0300

    Project created

---
+++ Swift-Netflix/Swift-Netflix/MainTabBarViewControllerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-Netflix
+//
+//  Created by Okan Orkun on 2.07.2023.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
